b8ecb58ba6ca6b51eacb4809f5d15011004bd1f9
HuMiao <humiao2006@gmail.com>
2021-08-11 18:36:38 +0800

    first commit

---
+++ src/renderer/layout/components/Sidebar/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+          :default-active="activeMenu"
+          :collapse="isCollapse"
+          :background-color="variables.menuBg"
+          :text-color="variables.menuText"
+          :unique-opened="false"
+          :active-text-color="variables.menuActiveText"
+          :collapse-transition="false"
+          mode="vertical"
+      >
+        <sidebar-item v-for="route in routers" :key="route.path" :item="route" :base-path="route.path"/>
+      </el-menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script>
+import SidebarItem from './SidebarItem'
+
+export default {
+  components: {SidebarItem},
+  data () {
+    return {
+      routers: [
+        {
+          path: '/'
+        }
+      ]
+    }
+  },
+  computed: {
+    activeMenu () {
+      return this.$route.path
+    }
+  }
+}
+</script>
---
+++ src/renderer/layout/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="app-wrapper">
+<!--    <sidebar class="sidebar-container"/>-->
+
+<!--    <app-main/>-->
+  </div>
+</template>
+
+<script>
+import { Sidebar } from './components'
+export default {
+  name: 'Layout',
+  components: {
+    Sidebar
+    // AppMain
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.app-wrapper {
+  position: relative;
+  height: 100%;
+  width: 100%;
+
+
+  .sidebar-container {
+    transition: width 0.28s;
+    width: 57px !important;
+    background-color: #304156;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 1001;
+    overflow: hidden;
+  }
+}
+
+</style>
